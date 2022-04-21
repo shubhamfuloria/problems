06956389fb750a710d24adfca70187c0d7007e62
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-04-21 23:11:12 +0530

    Time: 20 ms (5.33%), Space: 42.7 MB (5.88%) - LeetHub

---
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        if (root.left == null && root.right == null) {
+            return new ArrayList<>(Arrays.asList(root.val + ""));
+        }
+
+        List<String> leftLeafPaths = binaryTreePaths(root.left);
+        List<String> rightLeafPaths = binaryTreePaths(root.right);
+
+        List<String> res = new ArrayList<>();
+
+        for (String el : leftLeafPaths) {
+            String str = root.val + el + "";
+            res.add(str);
+        }
+
+        for (String el : rightLeafPaths) {
+            String str = root.val + el + "";
+            res.add(str);
+        }
+
+        return res;
+    }
+    public int sumNumbers(TreeNode root) {
+        
+        List<String>paths = binaryTreePaths(root);
+        int sum = 0;
+        
+        for(String el : paths) {
+            int curr = Integer.valueOf(el);
+            sum += curr;
+        }
+        
+        return sum;
+        
+    }
+}
