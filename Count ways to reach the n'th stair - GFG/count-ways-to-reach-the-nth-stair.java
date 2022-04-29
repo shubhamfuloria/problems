f2789e24938a217318e0b1cc0962832868e8883c
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-04-29 17:52:38 +0530

    Added solution - LeetHub

---
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total testcases
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    
+		    //taking count of stairs
+		    int m = sc.nextInt();
+		    
+		    //creating an object of class DynamicProgramming
+		    Solution obj = new Solution();
+		    
+		    //calling method countWays() of class
+		    //DynamicProgramming
+		    System.out.println(obj.countWays(m));
+		    
+		}
+		
+	}
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to count number of ways to reach the nth stair.
+    int countWays(int n) {
+        
+        int [] dp = new int[n + 1];
+        //each cell contains no of ways to reach to that cell from bottom
+        for(int i = 1; i < n + 1; i++) {
+            if(i == 1 || i == 2) {
+                dp[i] = i;
+            } else {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+            }
+        }
+        return dp[n];
+    }
+}
+
