39570b4910723ea713130ab2b713107646934c3b
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-04-29 15:54:47 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (60.47%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,19 +1,18 @@
 class Solution {
 
-    public int climbStairsMem(int n, int[] cache) {
-        if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 2;
-        } else if(cache[n] != 0) {
-            return cache[n];
-        }
-        
-
-        return cache[n] = climbStairsMem(n - 1, cache) + climbStairsMem(n - 2, cache);
-    }
-
     public int climbStairs(int n) {
-        return climbStairsMem(n, new int[n + 1]);
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            if (i == 2) {
+                dp[i] = 2;
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+
+        return dp[n];
     }
 }
