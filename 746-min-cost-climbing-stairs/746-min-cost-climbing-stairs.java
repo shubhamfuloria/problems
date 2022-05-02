bcc8e7c589623d1c11f344c86cebb61e6dd3f54c
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-05-02 17:26:05 +0530

    Time: 1 ms (82.07%), Space: 43.8 MB (43.65%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,27 +1,19 @@
 class Solution {
-        
-    public int climb(int[] cost, int n, int pos, Integer[] dp) {
-        
-        if(n == pos) {
-            return 0;
-        } else if(n < pos) {
-            return Integer.MAX_VALUE;
-        } 
-        
-        if(dp[pos] != null) {
-            return dp[pos];
-        }
-        
-        int op1 = climb(cost, n, pos + 1, dp);
-        int op2 = climb(cost, n, pos + 2, dp);
-        
-        return dp[pos] = Math.min(op1, op2) + cost[pos];
-    }
+
     public int minCostClimbingStairs(int[] cost) {
-    
-        //climb(n, 0) => returns the minimum cost to reach from 0th stair to nth stair
         int n = cost.length;
-        return Math.min(climb(cost, n, 0, new Integer[n + 1]), climb(cost, n, 1, new Integer[n + 1]));
-        
+        int[] dp = new int[n + 1];
+
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1) {
+                dp[i] = cost[i] + dp[i + 1];
+            } else {
+                dp[i] = Math.min(dp[i + 1], dp[i + 2]) + cost[i];
+            }
+        }
+
+        return Math.min(dp[0], dp[1]);
     }
 }
