2c0d1c7917daacbec2486181081bdbf96df71f05
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-05-02 22:49:56 +0530

    Time: 5 ms (15.90%), Space: 46.1 MB (35.03%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,26 +1,27 @@
 class Solution {
-    
-    
-    public int solve(int[][] grid, int i, int j, Integer[][] dp) {
-        
-        if(i == grid.length - 1 && j  == grid[0].length - 1) {
-            return grid[i][j];
-        } else if(i == grid.length || j == grid[0].length) {
-            return Integer.MAX_VALUE;
-        } else if(dp[i][j] != null) {
-            return dp[i][j];
+
+    public int minPathSum(int[][] cost) {
+        int m = cost.length;
+        int n = cost[0].length;
+
+        int[][] dp = new int[m][n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    dp[i][j] = cost[i][j];
+                } else if (i == m - 1) {
+                    // we cannot move to down
+                    dp[i][j] = dp[i][j + 1] + cost[i][j];
+                } else if (j == n - 1) {
+                    // we cannot move to right
+                    dp[i][j] = dp[i + 1][j] + cost[i][j];
+                } else {
+                    // we can move either side (right or bottom)
+                    dp[i][j] = Math.min(dp[i][j + 1], dp[i + 1][j]) + cost[i][j];
+                }
+            }
         }
-        
-        //moving right
-        int op1 = solve(grid, i, j + 1, dp);
-        //moving down
-        int op2 = solve(grid, i + 1, j, dp);
-        
-        
-        return dp[i][j] = Math.min(op1, op2) + grid[i][j];
-    }
-    public int minPathSum(int[][] grid) {
-        
-        return solve(grid, 0, 0, new Integer[grid.length + 1][grid[0].length + 1]);
+        return dp[0][0];
     }
 }
