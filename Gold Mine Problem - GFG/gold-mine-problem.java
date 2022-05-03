a0e8a4e2358af9976b58c28a372e59933e7dd348
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-05-03 09:45:20 +0530

    Added solution - LeetHub

--- Gold Mine Problem - GFG/gold-mine-problem.java
+++ Gold Mine Problem - GFG/gold-mine-problem.java
@@ -29,32 +29,37 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    public static int dig(int[][] mine, int i, int j, int[][] dp) {
-        
-        if (i < 0 || j < 0 || i == mine.length || j == mine[i].length) {
-          return 0;
-        } else if(dp[i][j] != 0) {
-            return dp[i][j];
-        }
-        
-        // digging top right diagonal
-        int op1 = dig(mine, i - 1, j + 1, dp);
-        // digging straight right
-        int op2 = dig(mine, i, j + 1, dp);
-        // digging down right diagonal
-        int op3 = dig(mine, i + 1, j + 1, dp);
-        
-        return dp[i][j] = Math.max(op1, Math.max(op2, op3)) + mine[i][j];
-    }
-    static int maxGold(int n, int m, int M[][]) {
-        // code her
-        int max_gold = 0;
+    static int maxGold(int n, int m, int mine[][]) {
+    
         int[][] dp = new int[n][m];
-        for(int i = 0; i < M.length; i++) {
-            int curr = dig(M, i, 0, dp);
-            max_gold = Math.max(max_gold, curr);
+    
+        // each cell of dp will contain max gold that can be achieved, if we start
+        // digging from that cell
+        // if we start digging from the last column , them the problem is smaller
+    
+        for (int j = m - 1; j >= 0; j--) {
+          for (int i = 0; i < n; i++) {
+    
+            if (j == m - 1) {
+              dp[i][j] = mine[i][j];
+            } else if(i == 0 && n == 1) {
+                dp[i][j] = dp[i][j + 1] + mine[i][j];  
+            } else if (i == 0 && n > 1) {
+              dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j + 1]) + mine[i][j];
+            } else if (i == n - 1) {
+              dp[i][j] = Math.max(dp[i][j + 1], dp[i - 1][j + 1]) + mine[i][j];
+            } else {
+              dp[i][j] = Math.max(dp[i - 1][j + 1], Math.max(dp[i][j + 1], dp[i + 1][j + 1])) + mine[i][j];
+            }
+          }
         }
-        
+        // we can start from any row of first column
+        int max_gold = 0;
+    
+        for (int i = 0; i < dp.length; i++) {
+          max_gold = Math.max(max_gold, dp[i][0]);
+        }
+    
         return max_gold;
     }
 }
