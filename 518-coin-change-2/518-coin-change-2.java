c59fd2bc635055e8776680f87e850c7b41a26b80
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-05-04 08:18:46 +0530

    Time: 3 ms (88.86%), Space: 41.7 MB (72.31%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -2,28 +2,14 @@ class Solution {
 
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < amount + 1; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else if (i == 0) {
-                    dp[i][j] = 0;
-                } else if (j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    if (coins[i - 1] <= j) {
-                        int exclude = dp[i - 1][j];
-                        // int include = Math.max(dp[i - 1][j - coins[i - 1]], dp[i][j - coins[i - 1]]);
-                        int include = dp[i][j - coins[i - 1]];
-                        dp[i][j] = include + exclude;
-                    } else {
-                        dp[i][j] = dp[i - 1][j];
-                    }
-                }
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j < amount + 1; j++) {
+                dp[j] += dp[j - coins[i]];
             }
         }
-        return dp[n][amount];
+
+        return dp[amount];
     }
 }
