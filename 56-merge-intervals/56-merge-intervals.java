b8539000b72aa7216ef07fd891c1c390d1e48df1
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-05-15 23:58:48 +0530

    Time: 15 ms (32.83%), Space: 54.7 MB (67.77%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, new Compare());
+
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] prevInterval = list.get(list.size() - 1);
+            int[] currInterval = intervals[i];
+            // now we need to check, if currInterval can be merged with prev interval
+            if (currInterval[0] <= prevInterval[1]) {
+                // we can merge
+                int newEnd = Math.max(prevInterval[1], currInterval[1]);
+                prevInterval[1] = newEnd;
+            } else {
+                list.add(currInterval);
+            }
+        }
+        int[][] res = new int[list.size()][2];
+        int i = 0;
+        for (int[] interval : list) {
+            res[i++] = interval;
+        }
+        return res;
+    }
+
+    static class Compare implements Comparator<int[]> {
+
+        public int compare(int[] a, int[] b) {
+            if (b[0] < a[0]) {
+                return 1;
+            } else if (a[0] < b[0]) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
