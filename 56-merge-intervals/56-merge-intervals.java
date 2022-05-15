1be9bb0c54372fe69fb10b8f4f5211b5904b2c5c
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-05-16 00:11:24 +0530

    Time: 13 ms (49.68%), Space: 55.2 MB (46.51%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -29,13 +29,7 @@ class Solution {
     static class Compare implements Comparator<int[]> {
 
         public int compare(int[] a, int[] b) {
-            if (b[0] < a[0]) {
-                return 1;
-            } else if (a[0] < b[0]) {
-                return -1;
-            } else {
-                return 0;
-            }
+            return Integer.compare(a[0], b[0]);
         }
     }
 }
