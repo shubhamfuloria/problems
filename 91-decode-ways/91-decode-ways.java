8478d788e09352e12ec7611881b0997b244a3c87
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-05-22 08:53:45 +0530

    Time: 4 ms (19.75%), Space: 43.3 MB (5.48%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,35 +1,35 @@
 class Solution {
 
-    public int numDecodings(String code) {
-        int[] dp = new int[code.length()];
-        
-        if(code.charAt(0) == '0') {
+    public int dfs(String S, HashMap<String, Integer> store) {
+        if (S.isEmpty()) {
+            return 1;
+        } else if (S.charAt(0) == '0') {
             return 0;
+        } else if (S.length() == 1) {
+            return 1;
+        } else if(store.containsKey(S)) {
+            return store.get(S);
         }
-        
-        dp[0] = 1;
 
-        for (int i = 1; i < dp.length; i++) {
-            char last_c = code.charAt(i);
-            char slast_c = code.charAt(i - 1);
+        int op1 = dfs(S.substring(1), store);
+        int op2 = 0;
 
-            if (last_c == '0' && slast_c == '0') {
-                dp[i] = 0;
-            } else if (slast_c == '0' && last_c != '0') {
-                dp[i] = dp[i - 1];
-            } else if (slast_c != '0' && last_c == '0') {
-                if (slast_c == '1' || slast_c == '2') {
-                    dp[i] = i >= 2 ? dp[i - 2] : 1;
-                } else {
-                    dp[i] = 0;
-                }
+        String firstTwo = S.substring(0, 2);
+
+        int num = Integer.parseInt(firstTwo);
+
+        if (num <= 26) {
+            if (S.length() == 2) {
+                op2 = 1;
             } else {
-                dp[i] = dp[i - 1];
-                if (Integer.parseInt(code.substring(i - 1, i + 1)) <= 26) {
-                    dp[i] += i >= 2 ? dp[i - 2] : 1;
-                }
+                op2 = dfs(S.substring(2), store);
             }
         }
-        return dp[code.length() - 1];
+        store.put(S, op1 + op2);
+        return op1 + op2;
+    }
+
+    public int numDecodings(String S) {
+        return dfs(S, new HashMap<String, Integer>());
     }
 }
