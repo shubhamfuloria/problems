8f7b7b5ec6791bef742cdc44a0013643baa6dedc
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-06-26 23:52:56 +0530

    Time: 1 ms (81.02%), Space: 41.9 MB (29.12%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -2,27 +2,20 @@ class Solution {
 
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> combs = new ArrayList<>();
-        backtrack(k, n, 1, new ArrayList<>(), combs);
+        // backtrack(k, n, 1, new ArrayList<>(), combs);
+        backtrack1(k, n, 1, 0, new ArrayList<>(), combs);
         return combs;
     }
 
-    public static void backtrack(int k, int n, int start, List<Integer> curr, List<List<Integer>> combs) {
-        if (curr.size() == k && sum(curr) == n) {
+    public static void backtrack1(int k, int n, int start, int sum, List<Integer> curr, List<List<Integer>> combs) {
+        if (curr.size() == k && sum == n) {
             combs.add(new ArrayList<>(curr));
         }
 
         for (int i = start; i <= 9; i++) {
             curr.add(i); // [1]
-            backtrack(k, n, i + 1, curr, combs);
+            backtrack1(k, n, i + 1, sum + i, curr, combs);
             curr.remove(curr.size() - 1); // []
         }
-    }
-
-    public static int sum(List<Integer> A) {
-        int sum = 0;
-        for (int el : A) {
-            sum += el;
-        }
-        return sum;
     }
 }
