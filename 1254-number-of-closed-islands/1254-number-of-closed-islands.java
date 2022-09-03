69b0e89ccedaf2067be739e142f11461579a21ae
Shubham Fuloria <50741125+shubhamfuloria@users.noreply.github.com>
2022-09-03 18:00:12 +0530

    Time: 7 ms (10.74%), Space: 50 MB (6.32%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -3,29 +3,32 @@ class Solution {
     public boolean isClosedIsland(int[][] grid, int row, int col) {
         if (row < 0 || col < 0 || row == grid.length || col == grid[0].length) {
             return false;
-        } else if (grid[row][col] == 1 || grid[row][col] == 2) {
+        } else if (grid[row][col] == 1 || grid[row][col] == -1) {
             return true;
         }
 
-        grid[row][col] = 2;
+        grid[row][col] = -1; // mark visited
 
-        boolean top = isClosedIsland(grid, row - 1, col);
-        boolean right = isClosedIsland(grid, row, col + 1);
-        boolean bottom = isClosedIsland(grid, row + 1, col);
-        boolean left = isClosedIsland(grid, row, col - 1);
-
-        return top && right && bottom && left;
+        int[][] dirs = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+            
+        boolean isClosed = true;
+        
+        for (int[] dir : dirs) {
+            isClosed = isClosedIsland(grid, row + dir[0], col + dir[1]) && isClosed;
+        }
+        return isClosed;
     }
 
     public int closedIsland(int[][] grid) {
         int count = 0;
-        for (int i = 1; i < grid.length - 1; i++) {
-            for (int j = 1; j < grid[0].length - 1; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 0 && isClosedIsland(grid, i, j)) {
                     count++;
                 }
             }
         }
+
         return count;
     }
 }
